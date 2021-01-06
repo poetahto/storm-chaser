0e11c6184fe94b8d038ed0147382f45d3280bc95
poetahto <walls.dk@gmail.com>
2021-01-06 08:34:50 -0800

    added level music

---
+++ storm chaser/Assets/Scripts/LevelMusic.cs
@@ -0,0 +1,31 @@
+﻿using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+[RequireComponent(typeof(LevelGameplayController))]
+public class LevelMusic : MonoBehaviour
+{
+    [SerializeField, EventRef] private string musicPath = null;
+
+    private static EventInstance _musicInstance;
+    private LevelGameplayController _gameplayController;
+    
+    private void Start()
+    {
+        _gameplayController = GetComponent<LevelGameplayController>();
+
+        if (!_musicInstance.hasHandle())
+        {
+            _musicInstance = RuntimeManager.CreateInstance(musicPath);
+            _musicInstance.start();    
+        }
+
+        _musicInstance.setParameterByName("Intensity", _gameplayController.PercentComplete);
+    }
+
+    private void Update()
+    {
+        _musicInstance.getParameterByName("Intensity", out var val);
+        _musicInstance.setParameterByName("Intensity", _gameplayController.PercentComplete);
+    }
+}
